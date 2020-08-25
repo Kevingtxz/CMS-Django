b3bbefab33803dbed30ec8193c3226415cb5464f
Kevin <kevingtxz@gmail.com>
2020-08-25 11:56:54 -0300

    003

--- api/accounts/views.py
+++ api/accounts/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import  HttpResponse
 
 def home(request):
-    return HttpResponse('home')
+    # The django will search for accounts/dashboard.html on api/accounts/templates
+    return render(request, 'accounts/dashboard.html')
 
 def products(request):
     return HttpResponse('products')
