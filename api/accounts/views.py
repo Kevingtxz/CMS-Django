156d8537e6b1c6c2baa9f8f942e0fcfc957cf4ee
Kevin <kevingtxz@gmail.com>
2020-08-30 13:30:42 -0300

    Bug on delete

--- api/accounts/views.py
+++ api/accounts/views.py
@@ -51,3 +51,8 @@ def updateOrder(request, pk):
             return redirect('/')
     context = {'form':form}
     return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    context = {'item':order}
+    return render(request, 'accounts/delete.html', context)
