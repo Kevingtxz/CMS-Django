45c64c6c7b5cf628b25ac7406e4ab0c8740c931c
Kevin <kevingtxz@gmail.com>
2020-08-31 12:46:00 -0300

    Fixing bugs

--- api/accounts/views.py
+++ api/accounts/views.py
@@ -32,18 +32,20 @@ def customer(request, pk_test):
     return render(request, 'accounts/customer.html', context)
 
 def createOrder(request, pk):
-    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'))
-    customer = Customer.objects.get(id=pk)
-    formset = OrderFormSet(instance=customer)
-    # form = OrderForm(initial={'customer':customer})
-    if request.method == "POST":
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/')
+	OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10 )
+	customer = Customer.objects.get(id=pk)
+	formset = OrderFormSet(queryset=Order.objects.none(),instance=customer)
+	#form = OrderForm(initial={'customer':customer})
+	if request.method == 'POST':
+		#print('Printing POST:', request.POST)
+		#form = OrderForm(request.POST)
+		formset = OrderFormSet(request.POST, instance=customer)
+		if formset.is_valid():
+			formset.save()
+			return redirect('/')
 
-    context = {'formset':formset}
-    return render(request, 'accounts/order_form.html', context)
+	context = {'form':formset}
+	return render(request, 'accounts/order_form.html', context)
 
 def updateOrder(request, pk):
     order = Order.objects.get(id=pk)
