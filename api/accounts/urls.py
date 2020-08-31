6326701fe6010253e12fe3ba00a31688f9bf6f63
Kevin <kevingtxz@gmail.com>
2020-08-31 10:42:56 -0300

    Fixing bug and styling delete

--- api/accounts/urls.py
+++ api/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name="home"),
     path('products/', views.products),
     path('customer/<str:pk_test>/', views.customer),
     path('create_order/', views.createOrder, name="create_order"),
