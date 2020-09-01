689ff4f0ba023e1e4b94549f085a28fdb0fc947d
Kevin <kevingtxz@gmail.com>
2020-09-01 13:55:18 -0300

    Search on customer.html

--- api/accounts/models.py
+++ api/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
@@ -9,20 +10,19 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
-    CATEGORY = (
-        ('Indoor', 'Indoor'),
-        ('Out door', 'Out door')
-    )
+    CATEGORY = (("Indoor", "Indoor"), ("Out door", "Out door"))
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
-    category =models.CharField(max_length=200, null=True, choices=CATEGORY)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     tags = models.ManyToManyField(Tag)
@@ -30,18 +30,19 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
 class Order(models.Model):
     STATUS = (
-        ('Pending', 'Pending'),
-        ('Out for delivery', 'Out for delivery'),
-        ('Delivered', 'Delivered')
+        ("Pending", "Pending"),
+        ("Out for delivery", "Out for delivery"),
+        ("Delivered", "Delivered"),
     )
 
-    customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
-    product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+    note = models.CharField(max_length=1000, null=True )
 
     def __str__(self):
         return self.product.name
-    
