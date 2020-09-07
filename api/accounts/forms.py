ce067cb1c5ed66e505f7611bb2847e8e982f2f62
Kevin <kevingtxz@gmail.com>
2020-09-07 16:12:10 -0300

    Start Login

--- api/accounts/forms.py
+++ api/accounts/forms.py
@@ -1,8 +1,22 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+
 from .models import Order
 
+
 class OrderForm(ModelForm):
-    class Meta:
-        model = Order
-        # __all__ -> return every attribute of Order
-        fields = '__all__'
+	class Meta:
+		model = Order
+		fields = '__all__'
+
+
+# NEW CODE *********
+class CreateUserForm(UserCreationForm):
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'password1', 'password2']
+
